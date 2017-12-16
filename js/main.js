ec3e6b32cd6c28174517eb43de9e80016248a272
dikshajoshi44 <dikshajoshi131@gmail.com>
2017-12-16 20:58:36 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 var x = 0;
-var array[{title:'',note:''}];
+var array=[{title:'',note:''}];
 function display_array(){
-    array[x].title=document.getElementById("formGroupExampleInput");
-    array[x].note=document.getElementById("formGroupExampleInput2");
-    console.log(array[x]);
+	if(x==0){
+	    array[x].title=document.getElementById("formGroupExampleInput").value;
+	    array[x].note=document.getElementById("formGroupExampleInput2").value;
+	    console.log(array[x]);
+
+
+	}
+	else {
+		var a=document.getElementById("formGroupExampleInput").value;
+			for(i=0;i<array.length;i++){
+				if(array[i].title==a){
+					window.alert("INVALID");
+				}
+			}
+	}
+
     x++;
 
     
