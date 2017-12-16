429ae1d030fcb02f438e4ba620b9cf0301239878
dikshajoshi44 <dikshajoshi131@gmail.com>
2017-12-16 17:38:19 +0530

    main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var x = 0;
+var array[{title:'blank',note:'abc'}];
+function display_array(){
+    array[x].title=document.getElementById("formGroupExampleInput");
+    array[x].note=document.getElementById("formGroupExampleInput2");
+    
+}
+
+
