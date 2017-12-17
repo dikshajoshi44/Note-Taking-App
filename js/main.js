5386edc7d7f79252162795652f9a52c618651ecd
dikshajoshi44 <dikshajoshi131@gmail.com>
2017-12-17 15:36:06 +0530

    display function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
 var notes=[];
-var x=0;
-function display_array(){
-	console.log(notes);
-	var noteObj = {};
-	var title=document.getElementById("formGroupExampleInput").value;
-	var note=document.getElementById("formGroupExampleInput2").value;
-
-	for (var i=0; i < notes.length; i++) {
-    	if (notes[i]['title'] == title) {
-      			alert('Already added');
-      			return;
+//var x=0;
+function addNote(){
+  console.log(notes);
+  var noteObj = {};
+  var title = document.getElementById('formGroupExampleInput').value;
+  var note = document.getElementById('formGroupExampleInput2').value;
+  for (var i=0; i < notes.length; i++) {
+    if (notes[i]['title'] == title) {
+      alert('Already added');
+      return;
     }
   }
+
+  noteObj['title'] = title;
+  noteObj['note'] = note;
+  notes.push(noteObj); 
+  displayNotes();
+}
+
+function updateNote(index) {
+  notes[index]['title'] = document.getElementById('formGroupExampleInput').value;
+  notes[index]['note'] = document.getElementById('formGroupExampleInput2').value;
+  displayNotes();
+}
+
+function prefillNotes(index) {
+  document.getElementById('addbutton').style.display = 'none';
+  document.getElementById('formGroupExampleInput').value = notes[index]['title'];
+  document.getElementById('formGroupExampleInput2').value = notes[index]['note'];
+  var updateButton = '<button id="update-note" onclick="updateNote('+index+');">Update Note</button>';
+  $('#form_id').append(updateButton);
+}
+
+function displayNotes() {
+  $(".row").html('');
+  for (var i=0; i< notes.length; i++) {
+      var htmlString = '<div class="col-4">' +
+      '<div class="card" style="width: 20rem;">' + '<i class="fa fa-pencil-square-o" aria-hidden="true" onclick="prefillNotes('+ i + ');">'
+      + '</i>' +
+       '<div class="card-body">' + 
+      '<h4 class="card-title">' + notes[i]['title'] + '</h4>' + '<p class="card-text">' + notes[i]['note'] + '</p>' + 
+      '</div>' +'</div>' +'</div>'  ;
+      $(".row").append(htmlString);
+      console.log(notes);
+  }
+}
+
+/*
 
   noteObj['title'] = title;
   noteObj['note'] = note;
@@ -36,7 +71,8 @@ function prefill_function(b){
     document.getElementById('formGroupExampleInput').value = notes[b]['title'];
     document.getElementById('formGroupExampleInput2').value = notes[b]['note'];
     document.getElementById('addbutton').style.display='none';
-    var htmlbutton='<button type="button" class="btn btn-primary" onclick="update_function('+ b +');">' +
+    var htmlbutton=
+    '<button type="button" class="btn btn-primary" onclick="update_function('+ b +');">' +
          Update + '</i>' +
         '</button>';
     $(".row").append(htmlbutton);
@@ -47,6 +83,54 @@ function update_function(b){
   console.log();
 
 }
+
+
+
+
+
+var notes = [];
+function addNote() {
+  console.log(notes);
+  var noteObj = {};
+  var title = document.getElementById('title').value;
+  var note = document.getElementById('note').value;
+  for (var i=0; i < notes.length; i++) {
+    if (notes[i]['title'] == title) {
+      alert('Already added');
+      return;
+    }
+  }
+  
+  noteObj['title'] = title;
+  noteObj['note'] = note;
+  notes.push(noteObj); 
+  displayNotes();
+}
+
+function updateNote(index) {
+  notes[index]['title'] = document.getElementById('title').value;
+  notes[index]['note'] = document.getElementById('note').value;
+  displayNotes();
+}
+
+function prefillNotes(index) {
+  document.getElementById('add-note').style.display = 'none';
+  document.getElementById('title').value = notes[index]['title'];
+  document.getElementById('note').value = notes[index]['note'];
+  var updateButton = '<button id="update-note" onclick="updateNote('+index+');">Update Note</button>';
+  $('#form').append(updateButton);
+}
+
+function displayNotes() {
+  $('#note-display').html('');
+  for (var i=0; i< notes.length; i++) {
+    var htmlString = '<p>' + notes[i]['title'] + ':' +
+        notes[i]['note'] + '</p>' + 
+        '<button onclick=prefillNotes(' + i + ')>Edit</button>';
+    $('#note-display').append(htmlString);
+  }
+}
+/*
 /*	if(array.length==0){
 		console.log();
 	    var str=dofunction();
